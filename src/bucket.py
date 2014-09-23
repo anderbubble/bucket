bfb37d290d5b9a1c6e1ac6a703fc20b21c878121
Jonathon Anderson <anderbubble@gmail.com>
2014-09-23 14:40:42 -0400

    Allow the override of the detected extension

--- src/bucket.py
+++ src/bucket.py
@@ -12,8 +12,7 @@ def main ():
     args = build_parser().parse_args()
     print 'bucket:', args.bucket
     for file_path in args.files:
-        mimetype, _ = mimetypes.guess_type(file_path)
-        extension = mimetypes.guess_extension(mimetype)
+        extension = guess_extension(file_path, args.extensions)
         digest = git_style_hash_object(file_path)
         bucket_path = '{path}{extension}'.format(
             path = os.path.join(*([args.bucket] + list(digest) + [digest])),
@@ -25,12 +24,29 @@ def main ():
         )
 
 
+def guess_extension (file_path, preferred_extensions=[]):
+    mimetype, _ = mimetypes.guess_type(file_path)
+    if preferred_extensions:
+        valid_extensions = [
+            extension.lower()
+            for extension in mimetypes.guess_all_extensions(mimetype)
+        ]
+        for extension in preferred_extensions:
+            if extension.lower() in valid_extensions:
+                return extension
+
+    return mimetypes.guess_extension(mimetype)
+
+
 def build_parser ():
     parser = argparse.ArgumentParser()
     parser.add_argument('files', metavar='FILE', nargs='+',
                         help='FILEs to store in the bucket')
     parser.add_argument('bucket', metavar='DIR',
                         help='DIRectory to use to store the bucket') 
+    parser.add_argument('-x', '--extension', dest='extensions',
+                        metavar='EXT', nargs='*',
+                        help='prefer these EXTensions when bucketing files')
     return parser
 
 
