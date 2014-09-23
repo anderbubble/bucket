ec73efed07dd63caf4dfce761bdb237c124fbe94
Jonathon Anderson <anderbubble@gmail.com>
2014-09-23 14:05:34 -0400

    Compute and print a sha1 hash of each file to bucket

--- src/bucket.py
+++ src/bucket.py
@@ -3,11 +3,15 @@
 
 
 import argparse
+import hashlib
+import os
 
 
 def main ():
     args = build_parser().parse_args()
-    print args
+    print 'bucket:', args.bucket
+    for file_path in args.files:
+        print git_style_hash_object(file_path), file_path
 
 
 def build_parser ():
@@ -17,6 +21,20 @@ def build_parser ():
     parser.add_argument('bucket', metavar='DIR',
                         help='DIRectory to use to store the bucket') 
     return parser
+
+
+def git_style_hash_object (file_path, blocksize=65536):
+    filesize = os.stat(file_path).st_size
+    sha1_hash = hashlib.sha1()
+    sha1_hash.update("blob {0}\0".format(filesize))
+    with open(file_path, 'rb') as file_:
+        while True:
+            buffer_ = file_.read(blocksize)
+            if not buffer_:
+                break
+            else:
+                sha1_hash.update(buffer_)
+    return sha1_hash.hexdigest()
 
 
 if __name__ == '__main__':
