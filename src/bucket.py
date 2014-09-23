0a9c900dd542df7c2cda3939b68944bcbcbdda54
Jonathon Anderson <anderbubble@gmail.com>
2014-09-23 15:27:12 -0400

    Actually copy files into the filebucket

--- src/bucket.py
+++ src/bucket.py
@@ -3,25 +3,50 @@
 
 
 import argparse
+import errno
 import hashlib
 import mimetypes
 import os
+import shutil
+import sys
 
 
 def main ():
     args = build_parser().parse_args()
-    print 'bucket:', args.bucket
     for file_path in args.files:
         extension = guess_extension(file_path, args.extensions)
         digest = git_style_hash_object(file_path)
-        bucket_path = '{path}{extension}'.format(
-            path = os.path.join(*([args.bucket] + list(digest) + [digest])),
-            extension = extension,
-        )
-        print '{source_file} -> {bucket_destination}'.format(
-            source_file = file_path,
-            bucket_destination = bucket_path,
-        )
+        bucket_relative_dir = os.path.join(*list(digest))
+        bucket_full_dir = os.path.join(args.bucket, bucket_relative_dir)
+        bucket_file_name = ''.join((digest, extension))
+        bucket_relative_path = os.path.join(bucket_relative_dir, bucket_file_name)
+        bucket_full_path = os.path.join(bucket_full_dir, bucket_file_name)
+        if args.verbose:
+            print >>sys.stderr, '{source_file} -> {bucket_destination}'.format(
+                source_file = file_path,
+                bucket_destination = bucket_full_path,
+            )
+        if not args.noop:
+            try:
+                mkdir_p(bucket_full_dir)
+            except OSError as ex:
+                print >>sys.stderr, '{0}: {1}'.format(ex.strerror, bucket_full_dir)
+                if not os.path.exists(bucket_full_dir):
+                    continue
+            shutil.copy(file_path, bucket_full_path)
+        print bucket_relative_path
+
+
+
+
+def mkdir_p (path):
+    try:
+        os.makedirs(path)
+    except OSError as ex:
+        if ex.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else:
+            raise
 
 
 def guess_extension (file_path, preferred_extensions=[]):
@@ -44,9 +69,17 @@ def build_parser ():
                         help='FILEs to store in the bucket')
     parser.add_argument('bucket', metavar='DIR',
                         help='DIRectory to use to store the bucket') 
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='display more information about where files will be bucketed')
     parser.add_argument('-x', '--extension', dest='extensions',
                         metavar='EXT', nargs='*',
                         help='prefer these EXTensions when bucketing files')
+    parser.add_argument('-n', '--noop', action='store_true',
+                        help='do not actually bucket any files')
+    parser.set_defaults(
+        noop = False,
+        verbose = False,
+    )
     return parser
 
 
